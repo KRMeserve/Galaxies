298e0b3a7455d666df3f48279d6df17e37481b7f
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-11-16 16:55:03 -0500

    Updated routes.rb

--- galaxies/config/routes.rb
+++ galaxies/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get '/galaxies', to: 'galaxies#index'
-
+  get 'galaxies/:id', to: 'galaxies#show'
+  post '/galaxies', to: 'galaxies#create'
+  delete '/galaxies/:id', to: 'galaxies#delete'
+  put '/galaxies/:id', to: 'galaxies#update'
 end
