d14e46db3b8991a0a55689d70f175aae5f734233
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-11-16 16:57:27 -0500

    Completed GalaxiesController

--- galaxies/app/controllers/galaxies_controller.rb
+++ galaxies/app/controllers/galaxies_controller.rb
@@ -2,7 +2,23 @@ class GalaxiesController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-        render json: Galaxy.all
-    end
+    render json: Galaxy.all
+  end
+
+  def show
+    render json: Galaxy.find(params["id"])
+  end
+
+  def create
+    render json: Galaxy.create(params["galaxy"])
+  end
+
+  def delete
+    render json: Galaxy.delete(params["id"])
+  end
+
+  def update
+    render json: Galaxy.update(params["id"], params["galaxy"])
+  end
 
 end
