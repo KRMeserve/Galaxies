4018a62539f614719c0800948085c5987aec7318
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-11-16 22:24:13 -0500

    added index.html and app.js and linked to react

---
+++ galaxies/public/js/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component{
+    render(){
+        return (
+            <h1>I'm inside the App class.</h1>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App></App>,
+    document.querySelector('main')
+)
