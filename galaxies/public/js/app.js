fdd2de173c91b723e3e16a11a369d103d59e9c7f
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-11-17 10:05:42 -0500

    react app now has working index page

--- galaxies/public/js/app.js
+++ galaxies/public/js/app.js
@@ -1,3 +1,42 @@
+class Galaxies extends React.Component {
+    constructor(props){
+        super(props);
+        this.getGalaxies = this.getGalaxies.bind(this)
+        this.state = {
+            galaxies: []
+        }
+    }
+    componentDidMount() {
+        this.getGalaxies();
+    }
+    getGalaxies() {
+        fetch('/galaxies').then(response => response.json()).then(data => {
+            this.setState({
+                galaxies: data
+            })
+            console.log(data);
+        })
+    }
+    render(){
+        return (
+            <div>
+                {this.state.galaxies.map((galaxy, index) => {
+                    return (
+                        <div className="galaxy">
+                            <h1>{galaxy.name}</h1>
+                            <h4>Distance from Earth: {galaxy.distance}</h4>
+                            <h4>Radius: {galaxy.radius}</h4>
+                            <h4>Date Discovered: {galaxy.date_discovered}</h4>
+                            <h4>Shape of Galaxy: {galaxy.shape}</h4>
+                            <img src={galaxy.img}></img>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
 class App extends React.Component{
     render(){
         return (
@@ -7,6 +46,6 @@ class App extends React.Component{
 }
 
 ReactDOM.render(
-    <App></App>,
+    <Galaxies></Galaxies>,
     document.querySelector('main')
 )
