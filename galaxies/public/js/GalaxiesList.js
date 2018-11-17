5850f6eff48921a818d25b10c6a37d69249c08bc
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-11-17 12:42:00 -0500

    have working show page

---
+++ galaxies/public/js/GalaxiesList.js
@@ -0,0 +1,16 @@
+class GalaxiesList extends React.Component {
+    render () {
+        return (
+              <div className="container">
+                  {this.props.galaxies.map((galaxy, index) => {
+                      return (
+                          <div className="galaxy">
+                          <img src={galaxy.img} onClick={()=>{this.props.showGalaxy(galaxy); this.props.toggleState('galaxiesIsVisible', 'galaxyIsVisible')}}></img>
+                              <h2>{galaxy.name}</h2>
+                          </div>
+                      )
+                  })}
+              </div>
+        )
+    }
+}
