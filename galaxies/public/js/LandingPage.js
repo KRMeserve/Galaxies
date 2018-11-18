45062df24ece18f3bb1d0178d71c56c3defd7956
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-11-18 12:21:12 -0500

    added base file for landing page

---
+++ galaxies/public/js/LandingPage.js
@@ -0,0 +1,9 @@
+class LandingPage extends React.Component{
+    render() {
+        return (
+            <div className='landingPageContainer'>
+                <h1>Landing Page</h1>
+            </div>
+        )
+    }
+}
